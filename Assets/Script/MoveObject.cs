4ee729e870ca172af6b8c261baff56feda6faa22
bong-seob-shin <56147393+bong-seob-shin@users.noreply.github.com>
2021-08-24 21:12:57 +0900

    오브젝트의 상위클래스 제작
    화면에서 움직이는 오브젝트들의 상위 클래스를 제작하여 기본적인 속성과 행동을 구현하였습니다.

--- Assets/Script/MoveObject.cs
+++ Assets/Script/MoveObject.cs
@@ -1,24 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
+    private float _HP;
+    private float _speed;
+    private float _damage;
     private float _width;
-
     private float _height;
+
     
-    
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 spriteSize = _spriteRenderer.size;
+        _width = spriteSize.x;
+        _height = spriteSize.y;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Move()
     {
         
     }
+
+    protected virtual void Dead()
+    {
+        
+    }
+
+    protected bool AABBCollisionCheck(Vector2 colPos)
+    {
+        float left = transform.position.x - _width / 2;
+        float right = transform.position.x + _width / 2;
+        float top = transform.position.y + _height / 2;
+        float bottom = transform.position.y - _height / 2;
+        
+        if (colPos.x < left || colPos.x > right)
+            return false;
+        if (colPos.y < bottom || colPos.y > top)
+            return false;
+        return true;
+    }
+
 }
